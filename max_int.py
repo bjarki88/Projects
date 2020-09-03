d750720a1ef7281d2dcae5df53760990231815dc
bjarkif19@ru.is <bjarkifridriksson@Bjarkis-MacBook-Pro.local>
2020-09-03 14:06:23 +0000

    changed max_int.py
    git commit -m Changed

--- max_int.py
+++ max_int.py
@@ -1,5 +1,5 @@
 
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
-max_int = num_int +1
+max_int = num_int + num_int
 print("The maximum is", max_int)    # Do not change this line
