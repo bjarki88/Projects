c8e1d9a0740748badc56fa3e9a2752f9cac47fff
bjarkif19@ru.is <bjarkifridriksson@Bjarkis-MacBook-Pro.local>
2020-09-03 14:43:10 +0000

    initial commit

--- max_int.py
+++ max_int.py
@@ -1,5 +1,7 @@
 
 num_int = int(input("Input a number: "))    # Do not change this line
 # Fill in the missing code
-max_int = num_int + num_int
+while num_int > 0:
+    num_int = int(input("Input a number: ")) 
+    max_int = max([num_int])
 print("The maximum is", max_int)    # Do not change this line
