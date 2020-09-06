72891530330b57074d7e51302a2f2876fd419a83
Bjarki Freyr <bjarkif19@ru.is>
2020-09-06 16:39:28 +0000

    Changes

--- verkefni3.py
+++ verkefni3.py
@@ -1,8 +1,12 @@
-flalb = int(input('Input f|a|b (fibonacci, abundant or both): '))
+flalb = input('Input f|a|b (fibonacci, abundant or both): ')
+if flalb == 'a':
+    max_num = int(input('Input the max number to check: '))
 
-start = 12
-if flalb < start:
-    print('Write a number above 12')
-for i in range(start,flalb+1):
-    if i % 2 == 0:
-        print(i)
+    sum_ = 0
+    for abundant_num in range(1,max_num+1):
+        for divisors in range(1, abundant_num):
+            if (abundant_num % divisors == 0):
+                sum_ = divisors + sum_
+        if sum_ > abundant_num:
+            print(abundant_num)
+        sum_ = 0
