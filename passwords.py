575350e09870567b551886553798355341ac6f83
Bjarki Freyr <bjarkif19@ru.is>
2020-09-14 23:21:57 +0000

    commit

--- passwords.py
+++ passwords.py
@@ -1,13 +1,13 @@
-pass_word = input('Please enter a password: ')
+pass_word = input('Please enter a password:')
 
 incorrect = 0
-valid = 0
+correct = 0
 
 while pass_word != 'q': # Create a constant so that if the user presses q it ends the loop
     upper_case = False
     lower_case = False
     number = False
-    if 6 < len(pass_word) < 20: # Create the range that the string must fall under
+    if 6 < len(pass_word) or len(pass_word) < 20: # Create the range that the string must fall under
         for i in pass_word:            # Create a for loop so that we can find what is missing or not missing in the input
             if i.isupper():
                 upper_case = True      # True if not q so that the loop continues 
@@ -23,13 +23,13 @@ while pass_word != 'q': # Create a constant so that if the user presses q it end
         if not number:
             print('At least one number needed')
         if number and lower_case and upper_case:
-            valid = valid + 1                # Create a correct count
+            correct = correct + 1                # Create a correct count
             print('Valid password of length', len(pass_word))
         else:
             incorrect = incorrect + 1     # Create an incorrect count
     else:
         print('Invalid length')
         incorrect = incorrect + 1
-pass_word = input('Enter a new password: ')
+    pass_word = input('Enter a new password: ')
 
-print('You tried {} passwords, {} correct, {} invalid'.format((valid + incorrect), valid, incorrect))
+print('You tried {} passwords, {} correct, {} invalid'.format((correct + incorrect), correct, incorrect))
