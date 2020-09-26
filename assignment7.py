6fe835471edca236c88c4b6c9502462787df7635
Bjarki Freyr <bjarkif19@ru.is>
2020-09-26 14:32:01 +0000

    first commit

---
+++ assignment7.py
@@ -0,0 +1,70 @@
+#def inc(string):
+    #result = string[::2]
+    #return result
+
+#input_str = input("Enter a string: ")
+#string_inc = inc(input_str)
+#print(string_inc)
+
+
+# Dæmi 2
+
+#def count_digits(a_str):
+#    count = 0
+#    for ch in a_str:
+#        if ch.isdigit():
+#            count = count + 1
+#    return count
+
+#input_str = input("Enter a string: ")
+#digit_count = count_digits(input_str)
+#print("No. of digits:", digit_count)
+
+
+# Dæmi 3
+
+#def max_of_three(a, b, c): 
+  
+    #if (a > b) and (a > c): 
+        #largest = a 
+  
+    #elif (b > a) and (b > c): 
+        #largest = b 
+    #else: 
+        #largest = c 
+          
+    #return largest
+    
+#first = int(input("Enter first number: "))
+#second = int(input("Enter second number: "))
+#third = int(input("Enter third number: "))
+
+#maximum = max_of_three(first,second,third)
+#print("Maximum:", maximum)
+
+# Dæmi 4
+
+#def is_prime(num):
+    #if (num==1):
+        #return False
+    #elif (num==2):
+        #return True
+    #else:
+        #for k in range(2,num):
+            #if(num % k) == 0:
+                #return False
+                #print(i,'is not a prime')
+        #else:
+            #return True
+            #print(i,'is a prime')
+
+    
+#max_num = int(input("Input an integer greater than 1: "))
+#for i in range(2,max_num+1):
+    #prime_num = is_prime(i)
+
+
+   
+
+
+
